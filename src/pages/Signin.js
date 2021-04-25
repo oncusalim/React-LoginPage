223e92630d63244fece8dba9bf1bcdf4a58f17de
oncusalim <salimoncu.fb@gmail.com>
2021-04-25 17:28:57 +0300

    Icon added to Signin Page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,24 +1,35 @@
 import React from 'react'
-import { Button, TextField, Grid, Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Button, TextField, Grid, Container, Avatar } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles} from '@material-ui/core/styles';
 import firebase from '../firebase/firebase.utils'
 import { Formik } from 'formik';
 import * as Yup from 'yup';
- 
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 
-const styles = makeStyles({
+
+const styles = makeStyles((theme)=>({
     wrapper: {
-    marginTop: "5rem",
+        marginTop: "5rem",
+        textAlign:'center'
     },
-  });
+    avatar: {
+        margin: "1rem auto",
+        backgroundColor: theme.palette.secondary.main
+    },
+    header:{
+        margin: "1rem auto",
+        
+    }
+}));
 
 function Signin() {
 
     const signinStyles = styles();
-    const initialValues  = {
+    const initialValues = {
         email: '',
-        password:'',
+        password: '',
     }
     const SigninSchema = Yup.object().shape({
         email: Yup.string().email('Invalid Email').required('Required'),
@@ -28,73 +39,78 @@ function Signin() {
             .required('Required')
 
     })
-    const handleFormSubmit = (values) =>{
+    const handleFormSubmit = (values) => {
         //alert(JSON.stringify(values, null, 2));
         firebase.signin(values.email, values.password)
     }
-   
 
-      const handleGoogleButtonClick = () =>{
-          firebase.useGoogleProvider();
-      }
+
+    const handleGoogleButtonClick = () => {
+        firebase.useGoogleProvider();
+    }
 
     return (
-        
+
         <div style={{ padding: 20 }}>
             <Container maxWidth="sm" className={signinStyles.wrapper}>
-               
-                <Formik 
+                <Avatar className={signinStyles.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h4" className={signinStyles.header}>
+                       Signin
+                </Typography>
+                <Formik
                     initialValues={initialValues}
                     validationSchema={SigninSchema}
                     onSubmit={handleFormSubmit}
                 >
                     {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         /* and other goodies */
-       }) => (
-                    <form onSubmit={handleSubmit}>
-           <Grid container spacing={6}>
-            
-             <Grid container item xs={12} spacing={3}>
-             <TextField 
-                name="email"
-                label="E-Mail" 
-                variant="outlined" 
-                fullWidth
-                value={values.email}
-                onChange={handleChange}
-                error={errors.email}
-                helperText={errors.email}
-                />
-             </Grid>
-             <Grid container item xs={12} spacing={3}>
-             <TextField 
-                name="password"
-                label="Password" 
-                variant="outlined" 
-                type="password"
-                fullWidth
-                value={values.password}
-                onChange={handleChange}
-                error={errors.password}
-                helperText={errors.password}
-                />
-             </Grid>
-             <Grid container item xs={12} spacing={3}>
-            <Button variant="contained" color="primary" fullWidth type="submit">Login</Button>
-            </Grid>
-            <Grid container item xs={12} spacing={3}>
-            <Button variant="contained" color="primary" fullWidth onClick={handleGoogleButtonClick}>SignUp With Google</Button>
-            </Grid>
-            </Grid>
-            </form> 
-            )}
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                        /* and other goodies */
+                    }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Grid container spacing={6}>
+
+                                <Grid container item xs={12} spacing={3}>
+                                    <TextField
+                                        name="email"
+                                        label="E-Mail"
+                                        variant="outlined"
+                                        fullWidth
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        error={errors.email}
+                                        helperText={errors.email}
+                                    />
+                                </Grid>
+                                <Grid container item xs={12} spacing={3}>
+                                    <TextField
+                                        name="password"
+                                        label="Password"
+                                        variant="outlined"
+                                        type="password"
+                                        fullWidth
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        error={errors.password}
+                                        helperText={errors.password}
+                                    />
+                                </Grid>
+                                <Grid container item xs={12} spacing={3}>
+                                    <Button variant="contained" color="primary" fullWidth type="submit">Login</Button>
+                                </Grid>
+                                <Grid container item xs={12} spacing={3}>
+                                    <Button variant="contained" color="primary" fullWidth onClick={handleGoogleButtonClick}>SignUp With Google</Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    )}
                 </Formik>
             </Container>
         </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,9 +7,9 @@ import * as Yup from 'yup';
 
 const styles = makeStyles({
     wrapper: {
-    marginTop: "5rem",
+        marginTop: "5rem",
     },
-  });
+});
 
 function Signup() {
 
@@ -27,72 +27,72 @@ function Signup() {
 
     const formik = useFormik({
         initialValues: {
-            displayName:'',
+            displayName: '',
             email: '',
-            password:'',
+            password: '',
         },
         validationSchema: SignUpSchema,
         onSubmit: values => {
-         // alert(JSON.stringify(values, null, 2));
+            // alert(JSON.stringify(values, null, 2));
             firebase.register(values.displayName, values.email, values.password)
         },
-      });
+    });
 
-      const handleGoogleButtonClick = () =>{
-          firebase.useGoogleProvider();
-      }
+    const handleGoogleButtonClick = () => {
+        firebase.useGoogleProvider();
+    }
 
     return (
-        
+
         <div style={{ padding: 20 }}>
             <Container maxWidth="sm" className={signupStyles.wrapper}>
                 <form onSubmit={formik.handleSubmit} validationSchema={SignUpSchema}>
-           <Grid container spacing={6}>
-            <Grid container item xs={12} spacing={3}>
-             <TextField 
-                name="displayName"
-                label="Display Name" 
-                variant="outlined" 
-                fullWidth
-                value={formik.values.displayName}
-                onChange={formik.handleChange}
-                error={formik.errors.displayName}
-                helperText={formik.errors.displayName}
-                />
-             </Grid>
-             <Grid container item xs={12} spacing={3}>
-             <TextField 
-                name="email"
-                label="E-Mail" 
-                variant="outlined" 
-                fullWidth
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                error={formik.errors.email}
-                helperText={formik.errors.email}
-                />
-             </Grid>
-             <Grid container item xs={12} spacing={3}>
-             <TextField 
-                name="password"
-                label="Password" 
-                variant="outlined" 
-                type="password"
-                fullWidth
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                error={formik.errors.password}
-                helperText={formik.errors.password}
-                />
-             </Grid>
-             <Grid container item xs={12} spacing={3}>
-            <Button variant="contained" color="primary" fullWidth type="submit">Register</Button>
-            </Grid>
-            <Grid container item xs={12} spacing={3}>
-            <Button variant="contained" color="primary" fullWidth onClick={handleGoogleButtonClick}>SignUp With Google</Button>
-            </Grid>
-            </Grid>
-            </form>
+                    <Grid container spacing={6}>
+                        <Grid container item xs={12} spacing={3}>
+                            <TextField
+                                name="displayName"
+                                label="Display Name"
+                                variant="outlined"
+                                fullWidth
+                                value={formik.values.displayName}
+                                onChange={formik.handleChange}
+                                error={formik.errors.displayName}
+                                helperText={formik.errors.displayName}
+                            />
+                        </Grid>
+                        <Grid container item xs={12} spacing={3}>
+                            <TextField
+                                name="email"
+                                label="E-Mail"
+                                variant="outlined"
+                                fullWidth
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                error={formik.errors.email}
+                                helperText={formik.errors.email}
+                            />
+                        </Grid>
+                        <Grid container item xs={12} spacing={3}>
+                            <TextField
+                                name="password"
+                                label="Password"
+                                variant="outlined"
+                                type="password"
+                                fullWidth
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                error={formik.errors.password}
+                                helperText={formik.errors.password}
+                            />
+                        </Grid>
+                        <Grid container item xs={12} spacing={3}>
+                            <Button variant="contained" color="primary" fullWidth type="submit">Register</Button>
+                        </Grid>
+                        <Grid container item xs={12} spacing={3}>
+                            <Button variant="contained" color="primary" fullWidth onClick={handleGoogleButtonClick}>SignUp With Google</Button>
+                        </Grid>
+                    </Grid>
+                </form>
             </Container>
         </div>
     )
